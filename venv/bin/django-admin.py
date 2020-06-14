6135c6c05142fb57eb82ffd10e2863f7eb6da698
Lakshay Sagar Rana <lakshaysagarrana@gmail.com>
2020-06-15 01:19:47 +0530

    [Added] Initiaized Porject

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lakshaytalksto/Downloads/Git-enabled-projects/DjangoDisect/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
